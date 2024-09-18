746df2948084fd5d95d9014f6b4a3265ce154cdb
eplato12 <eplatonov@middlebury.edu>
2024-09-18 16:50:37 +0200

    water goes both ways

--- frog game/Assets/Scripts/WaterRotation.cs
+++ frog game/Assets/Scripts/WaterRotation.cs
@@ -6,7 +6,7 @@ public class WaterRotation : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        speed = Random.Range(1, 10);
+        speed = Random.Range(-10, 10);
     }
 
     // Update is called once per frame
