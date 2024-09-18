5e576cc8afd18992356ca1acc3ddd8609fc24b42
wkl12-k <wlau@bowdoin.edu>
2024-09-18 16:29:03 +0200

    timer?

--- frog game/Assets/Scripts/Timer.cs
+++ frog game/Assets/Scripts/Timer.cs
@@ -2,42 +2,35 @@ using System.Collections;
 using UnityEngine.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
-public class StartGame : MonoBehaviour
+public class Timer : MonoBehaviour
 {
 
-    public float timeLeft;
-    public Text timerText;
-    public bool timerOn;
-
-    void Start()
-    {
-        timerOn = true;
-    }
+    [SerializeField] float timeLeft;
+    [SerializeField] TextMeshProUGUI timerText;
 
     void Update()
     {
-        if (timerOn)
+        if (timerText == null)
         {
-            if (timeLeft > 0)
-            {
-               timeLeft -= Time.deltaTime;
-               updateTimer(timeLeft);
-            }
+            Debug.LogError("Timer Text is not assigned!");
+            return;
         }
-        else
+
+        // Your existing code
+        if (timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+        }
+        else if (timeLeft < 0)
         {
             timeLeft = 0;
-            timerOn = false;
         }
-    
-    }
 
-    void updateTimer(float currentTime)
-    {
-        currentTime += 1;
-        float minutes = Mathf.FloorToInt(currentTime / 60);
-        float seconds = Mathf.FloorToInt(currentTime % 60);
+        int minutes = Mathf.FloorToInt(timeLeft / 60);
+        int seconds = Mathf.FloorToInt(timeLeft % 60);
         timerText.text = string.Format("{0:00} : {1:00}", minutes, seconds);
     }
+
 }
