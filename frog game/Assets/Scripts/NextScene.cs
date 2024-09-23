d97cee30f4f95ef52a432520000c7fcb2dc0637d
eplato12 <eplatonov@middlebury.edu>
2024-09-23 21:59:05 +0200

    pause and exit buttons work!

--- frog game/Assets/Scripts/NextScene.cs
+++ frog game/Assets/Scripts/NextScene.cs
@@ -3,8 +3,52 @@ using UnityEngine.SceneManagement;
 
 public class AdvanceScene : MonoBehaviour
 {
+    public GameObject exitPanel;
+    public GameObject playButton;
+
     public void LoadNextScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void openExitPanel()
+    {
+        if(exitPanel != null)
+        {
+            exitPanel.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    public void closeExitPanel()
+    {
+        if (exitPanel != null)
+        {
+            exitPanel.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
+    public void toLevelMenu()
+    {
+        SceneManager.LoadScene("Level Menu");
+    }
+
+    public void pause()
+    {
+        if (playButton != null)
+        {
+            playButton.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    public void play()
+    {
+        if (playButton != null)
+        {
+            playButton.SetActive(false);
+            Time.timeScale = 1;
+        }
     }
 }
