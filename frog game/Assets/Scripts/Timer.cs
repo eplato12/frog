04372cc685f07ac0b8e586892d5cd2605707b5cd
wkl12-k <wlau@bowdoin.edu>
2024-09-26 16:21:52 +0200

    time logic

--- frog game/Assets/Scripts/Timer.cs
+++ frog game/Assets/Scripts/Timer.cs
@@ -21,12 +21,19 @@ public class Timer : MonoBehaviour
         else if (timeLeft < 0)
         {
             timeLeft = 0;
-            SceneManager.GetActiveScene();
+            ReloadCurrentScene();
         }
 
         int minutes = Mathf.FloorToInt(timeLeft / 60);
         int seconds = Mathf.FloorToInt(timeLeft % 60);
         timerText.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+    }
+
+    private void ReloadCurrentScene()
+    {
+        // Get the active scene and reload it
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.name);
     }
 
 }
