9437ba1c28623bce86abc53ae1bca767aa050450
Elene Sturua <sturuael@grinnell.edu>
2024-09-29 22:14:12 +0200

    ok

--- frog game/Assets/Scripts/SpawnLilyPads.cs
+++ frog game/Assets/Scripts/SpawnLilyPads.cs
@@ -1,11 +1,13 @@
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class SpawnLilyPads : MonoBehaviour
 
-   
+
 {
-   public GameObject evilLilies;
+    public GameObject[] evilLillies;
    public GameObject[] waterRings;
     public GameObject[] lillies;
     public int level;
@@ -30,13 +32,16 @@ public class SpawnLilyPads : MonoBehaviour
         }
         else if (level == 3)
         {
-            SpawnLevel3();
+            StartCoroutine(SpawnLevel3());
         }
         else if (level == 4)
         {
             SpawnLevel4();
         }
+        
     }
+
+       
 
 
     private void SpawnLevel1()
@@ -105,48 +110,62 @@ public class SpawnLilyPads : MonoBehaviour
 
     private IEnumerator SpawnLevel3()
     {
-        // get radius of each circle by position of first lillies
+        // Get radius of each circle by position of first lillies
         float[] radii = { lillies[0].transform.position.x, lillies[1].transform.position.x, lillies[2].transform.position.x };
+        List<GameObject> spawnedLillies = new System.Collections.Generic.List<GameObject>();
 
-        // loop through all three layers
+        // Loop through all three layers
         for (int i = 0; i < numLillies.Length; i++)
         {
-            // to make the correct number of lillies in each layer
+            // Spawn the correct number of lillies in each layer
             for (int j = 1; j < numLillies[i]; j++)
             {
-                // get x and y position of new lily (depends on number of lillies)
+                // Calculate x and y position of the new lily based on the number of lillies
                 float xPos = radii[i] * Mathf.Cos(2 * j * Mathf.PI / numLillies[i]);
                 float yPos = radii[i] * Mathf.Sin(2 * j * Mathf.PI / numLillies[i]);
 
-                GameObject lily = null;
+                GameObject lily = Instantiate(lillies[i], new Vector3(xPos, yPos, 1), Quaternion.identity);
 
-                // choose what to spawn (lily, water, etc.)
-                float probLily = Random.Range(0, 10);
-                if (probLily < 7)
-                {
-                    lily = lillies[i];
-                }
+                // Set it as a child of the water ring to make it rotate
+                lily.transform.parent = waterRings[i].transform;
+                lily.transform.localScale = lilyScales[i];
 
-                if (lily != null)
-                {
-                    // instantiate that object 
-                    lily = Instantiate(lillies[i], new Vector3(xPos, yPos, 1), Quaternion.identity);
+                // Add lily to the list
+                spawnedLillies.Add(lily);
 
-                    // set it as a child of the water rings to make it rotate 
-                    lily.transform.parent = waterRings[i].transform;
-                    lily.transform.localScale = lilyScales[i];
-
-                    yield return new WaitForSeconds(2f);
-                    GameObject evilLilySpawn = Instantiate(evilLilies, new Vector3(xPos, yPos, 1), Quaternion.identity);
-                    lily.GetComponent<Renderer>().enabled = false;
-
-                    yield return new WaitForSeconds(1f);
-                        Destroy(lily);
-
-                }
+                // Short delay before spawning the next lily
+                yield return new WaitForSeconds(0.1f);
             }
         }
+
+        // Wait for a short time before replacing some lilies with evil lilies
+        yield return new WaitForSeconds(2f);
+
+        // Shuffle the list and select approximately 1/3 of them
+        int numToReplace = spawnedLillies.Count / 3;
+        System.Random rand = new System.Random();
+        spawnedLillies = spawnedLillies.OrderBy(x => rand.Next()).ToList();
+
+        for (int k = 0; k < numToReplace; k++)
+        {
+            GameObject originalLily = spawnedLillies[k];
+            Vector3 position = originalLily.transform.position;
+
+            // Get the index `i` of the layer this lily belongs to for evilLily instantiation
+            int layerIndex = originalLily.transform.parent.GetSiblingIndex();
+
+            // Spawn the evilLily in the same position
+            GameObject evilLily = Instantiate(evilLillies[layerIndex], position, Quaternion.identity);
+            evilLily.transform.parent = waterRings[layerIndex].transform;
+            evilLily.transform.localScale = lilyScales[layerIndex];
+
+            // Destroy the original lily
+            Destroy(originalLily);
+        }
     }
+
+
+
 
 
 
