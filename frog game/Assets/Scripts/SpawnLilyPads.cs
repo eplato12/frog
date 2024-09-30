f88e6a996bdac72cc6f415134ababc6d47e93907
eplato12 <eplatonov@middlebury.edu>
2024-09-30 22:07:06 +0200

    bg colors

--- frog game/Assets/Scripts/SpawnLilyPads.cs
+++ frog game/Assets/Scripts/SpawnLilyPads.cs
@@ -34,7 +34,6 @@ public class SpawnLilyPads : MonoBehaviour
         }
         else if (level == 2)
         {
-            portal.SetActive(false);
             SpawnLevel2();
         }
         else if (level == 3)
@@ -44,6 +43,11 @@ public class SpawnLilyPads : MonoBehaviour
         else if (level == 4)
         {
             SpawnLevel4();
+        }
+        else if (level == 5)
+        {
+            SpawnLevel5();
+            portal.SetActive(false);
         }
     }
 
@@ -75,9 +79,6 @@ public class SpawnLilyPads : MonoBehaviour
 
     private void SpawnLevel2()
     {
-        // create a list to store the lily game objects to spawn stars on
-        List<GameObject> spawnedLillies = new List<GameObject>();
-
         // get radius of each circle by position of first lillies
         float[] radii = {lillies[0].transform.position.x, lillies[1].transform.position.x, lillies[2].transform.position.x };
 
@@ -108,22 +109,8 @@ public class SpawnLilyPads : MonoBehaviour
                     // set it as a child of the water rings to make it rotate 
                     lily.transform.parent = waterRings[i].transform;
                     lily.transform.localScale = lilyScales[i];
-
-                    spawnedLillies.Add(lily);
                 }
-
-                
             }
-        }
-
-        // spawn stars 
-        for (int k = 0; k < 2; k++)
-        {
-            // randomly select lillies to spawn a star on
-            GameObject starLily1 = spawnedLillies[(int)Mathf.Floor(Random.Range(0, spawnedLillies.Count - 1))];
-            // spawn a star on that lily 
-            GameObject star1 = Instantiate(star, starLily1.transform.position, Quaternion.identity);
-            star1.transform.parent = starLily1.transform;
         }
     }
 
@@ -235,6 +222,58 @@ public class SpawnLilyPads : MonoBehaviour
             // create a portal at that location
             newPortal = Instantiate(portal, portalLily.transform.position, Quaternion.identity);
             newPortal.transform.parent = portalLily.transform.parent;
+        }
+    }
+
+    private void SpawnLevel5()
+    {
+        // create a list to store the lily game objects to spawn stars on
+        List<GameObject> spawnedLillies = new List<GameObject>();
+
+        // get radius of each circle by position of first lillies
+        float[] radii = { lillies[0].transform.position.x, lillies[1].transform.position.x, lillies[2].transform.position.x };
+
+        // loop through all three layers
+        for (int i = 0; i < numLillies.Length; i++)
+        {
+            // to make the correct number of lillies in each layer
+            for (int j = 1; j < numLillies[i]; j++)
+            {
+                // get x and y position of new lily (depends on number of lillies)
+                float xPos = radii[i] * Mathf.Cos(2 * j * Mathf.PI / numLillies[i]);
+                float yPos = radii[i] * Mathf.Sin(2 * j * Mathf.PI / numLillies[i]);
+
+                GameObject lily = null;
+
+                // choose what to spawn (lily, water, etc.)
+                float probLily = Random.Range(0, 10);
+                if (probLily < 8.5)
+                {
+                    lily = lillies[i];
+                }
+
+                if (lily != null)
+                {
+                    // instantiate that object 
+                    lily = Instantiate(lillies[i], new Vector3(xPos, yPos, 1), Quaternion.identity);
+
+                    // set it as a child of the water rings to make it rotate 
+                    lily.transform.parent = waterRings[i].transform;
+                    lily.transform.localScale = lilyScales[i];
+
+                    spawnedLillies.Add(lily);
+                }
+            }
+        }
+
+        // spawn stars 
+        for (int k = 0; k < 2; k++)
+        {
+            // randomly select lillies to spawn a star on
+            GameObject starLily1 = spawnedLillies[(int)Mathf.Floor(Random.Range(0, spawnedLillies.Count - 1))];
+            // spawn a star on that lily 
+            GameObject star1 = Instantiate(star, starLily1.transform.position, Quaternion.identity);
+            star1.transform.parent = starLily1.transform;
         }
     }
 }
