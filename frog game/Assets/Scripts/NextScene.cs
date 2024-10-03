e9b18a0b7058474b449b914fc60f3decf5d6853d
wkl12-k <wlau@bowdoin.edu>
2024-10-03 11:24:49 +0200

    Revert "Merge branch 'main' of https://github.com/eplato12/frog"
    This reverts commit c7cd38a9c1233f51cadadb9c5fbd98ad904039b0, reversing
    changes made to f3a85bf6e49d7650f7863a5b82cf99fb3f6a31af.

--- frog game/Assets/Scripts/NextScene.cs
+++ frog game/Assets/Scripts/NextScene.cs
@@ -3,57 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class AdvanceScene : MonoBehaviour
 {
-    public GameObject exitPanel;
-    public GameObject playButton;
-
-    public static void LoadNextScene()
+    public void LoadNextScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-
-    public void openExitPanel()
-    {
-        if(exitPanel != null)
-        {
-            exitPanel.SetActive(true);
-            Time.timeScale = 0;
-        }
-    }
-
-    public void closeExitPanel()
-    {
-        if (exitPanel != null)
-        {
-            exitPanel.SetActive(false);
-            Time.timeScale = 1;
-        }
-    }
-
-    public void toLevelMenu()
-    {
-        SceneManager.LoadScene("Level Menu");
-    }
-
-    public void pause()
-    {
-        if (playButton != null)
-        {
-            playButton.SetActive(true);
-            Time.timeScale = 0;
-        }
-    }
-
-    public void play()
-    {
-        if (playButton != null)
-        {
-            playButton.SetActive(false);
-            Time.timeScale = 1;
-        }
-    }
-
-    public void toLevel(string name)
-    {
-        SceneManager.LoadScene(name);
     }
 }
