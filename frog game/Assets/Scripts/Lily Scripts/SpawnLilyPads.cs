9b245402fdd5cce00bf04f3d5724bdced3a1c9f1
Elene Sturua <sturuael@grinnell.edu>
2024-10-05 21:24:00 +0200

    script neat

--- frog game/Assets/Scripts/Lily Scripts/SpawnLilyPads.cs
+++ frog game/Assets/Scripts/Lily Scripts/SpawnLilyPads.cs
@@ -20,13 +20,13 @@ public class SpawnLilyPads : MonoBehaviour
     private lily Lily;
 
 
-    private int[] numLillies = { 20, 14, 7 }; // array containing number of lillies for each water ring
+    private int[] numLillies = { 20, 14, 7 }; 
     private Vector3[] lilyScales = {
             new Vector3((float)0.3328913, (float)0.328198, 1),
             new Vector3((float)0.3505113, (float)0.3406977, 1),
-            new Vector3((float)0.4058551, (float)0.394492, 1)}; // array containing the local scales of each lily
+            new Vector3((float)0.4058551, (float)0.394492, 1)}; 
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
 
     void Start()
     {
@@ -53,25 +53,26 @@ public class SpawnLilyPads : MonoBehaviour
     }
 
 
+
     private void SpawnLevel1()
     {
-        // get radius of each circle by position of first lillies
-        float[] radii = { lillies[0].transform.position.x, lillies[1].transform.position.x, lillies[2].transform.position.x };
+        
+        float[] circleRadii = { lillies[0].transform.position.x, lillies[1].transform.position.x, lillies[2].transform.position.x };
 
-        // loop through all three layers
+        
         for (int i = 0; i < numLillies.Length; i++)
         {
-            // to make the correct number of lillies in each layer
+            
             for (int j = 1; j < numLillies[i]; j++)
             {
-                // get x and y position of new lily (depends on number of lillies)
-                float xPos = radii[i] * Mathf.Cos(2 * j * Mathf.PI / numLillies[i]);
-                float yPos = radii[i] * Mathf.Sin(2 * j * Mathf.PI / numLillies[i]);
+                
+                float xPosLily = circleRadii[i] * Mathf.Cos(2 * j * Mathf.PI / numLillies[i]);
+                float yPosLily = circleRadii[i] * Mathf.Sin(2 * j * Mathf.PI / numLillies[i]);
 
-                // instantiate that object 
-                GameObject lily = Instantiate(lillies[i], new Vector3(xPos, yPos, 1), Quaternion.identity);
+                
+                GameObject lily = Instantiate(lillies[i], new Vector3(xPosLily, yPosLily, 1), Quaternion.identity);
 
-                // set it as a child of the water rings to make it rotate 
+                
                 lily.transform.parent = waterRings[i].transform;
                 lily.transform.localScale = lilyScales[i];
             }
@@ -80,34 +81,34 @@ public class SpawnLilyPads : MonoBehaviour
 
     private void SpawnLevel2()
     {
-        // get radius of each circle by position of first lillies
-        float[] radii = { lillies[0].transform.position.x, lillies[1].transform.position.x, lillies[2].transform.position.x };
+        
+        float[] circleRadii = { lillies[0].transform.position.x, lillies[1].transform.position.x, lillies[2].transform.position.x };
 
-        // loop through all three layers
+        
         for (int i = 0; i < numLillies.Length; i++)
         {
-            // to make the correct number of lillies in each layer
+            
             for (int j = 1; j < numLillies[i]; j++)
             {
-                // get x and y position of new lily (depends on number of lillies)
-                float xPos = radii[i] * Mathf.Cos(2 * j * Mathf.PI / numLillies[i]);
-                float yPos = radii[i] * Mathf.Sin(2 * j * Mathf.PI / numLillies[i]);
+                
+                float xPosLily = circleRadii[i] * Mathf.Cos(2 * j * Mathf.PI / numLillies[i]);
+                float yPosLily = circleRadii[i] * Mathf.Sin(2 * j * Mathf.PI / numLillies[i]);
 
                 GameObject lily = null;
 
-                // choose what to spawn (lily, water, etc.)
-                float probLily = Random.Range(0, 10);
-                if (probLily < 7)
+                
+                float lilySpawnProbability = Random.Range(0, 10);
+                if (lilySpawnProbability < 7)
                 {
                     lily = lillies[i];
                 }
 
                 if (lily != null)
                 {
-                    // instantiate that object 
-                    lily = Instantiate(lillies[i], new Vector3(xPos, yPos, 1), Quaternion.identity);
+                    
+                    lily = Instantiate(lillies[i], new Vector3(xPosLily, yPosLily, 1), Quaternion.identity);
 
-                    // set it as a child of the water rings to make it rotate 
+                    
                     lily.transform.parent = waterRings[i].transform;
                     lily.transform.localScale = lilyScales[i];
                 }
@@ -117,39 +118,39 @@ public class SpawnLilyPads : MonoBehaviour
 
     private void SpawnLevel3()
     {
-        // get radius of each circle by position of first lillies
-        float[] radii = { lillies[0].transform.position.x, lillies[1].transform.position.x, lillies[2].transform.position.x };
+        
+        float[] circleRadii = { lillies[0].transform.position.x, lillies[1].transform.position.x, lillies[2].transform.position.x };
 
-        // loop through all three layers
+        
         for (int i = 0; i < numLillies.Length; i++)
         {
-            // to make the correct number of lillies in each layer
+            
             for (int j = 1; j < numLillies[i]; j++)
             {
-                // get x and y position of new lily (depends on number of lillies)
-                float xPos = radii[i] * Mathf.Cos(2 * j * Mathf.PI / numLillies[i]);
-                float yPos = radii[i] * Mathf.Sin(2 * j * Mathf.PI / numLillies[i]);
+                
+                float xPosLily = circleRadii[i] * Mathf.Cos(2 * j * Mathf.PI / numLillies[i]);
+                float yPosLily = circleRadii[i] * Mathf.Sin(2 * j * Mathf.PI / numLillies[i]);
 
                 GameObject lily = null;
 
-                // choose what to spawn (lily, water, etc.)
-                float probLily = Random.Range(0, 10);
-                if (probLily < 7)
+                
+                float lilySpawnProbability = Random.Range(0, 10);
+                if (lilySpawnProbability < 7)
                 {
                     lily = lillies[i];
                 }
                 if (lily != null)
                 {
-                    // instantiate that object 
-                    lily = Instantiate(lillies[i], new Vector3(xPos, yPos, 1), Quaternion.identity);
+                    
+                    lily = Instantiate(lillies[i], new Vector3(xPosLily, yPosLily, 1), Quaternion.identity);
 
-                    // set it as a child of the water rings to make it rotate 
+                     
                     lily.transform.parent = waterRings[i].transform;
                     lily.transform.localScale = lilyScales[i];
 
                     Lily = lily.GetComponent<lily>();
 
-                    bool isEvil = Random.Range(0, 10) < 3;
+                    bool isEvil = Random.Range(0, 10) < 7;
 
                     Lily.SetIsEvil(isEvil);
                 }
@@ -160,43 +161,43 @@ public class SpawnLilyPads : MonoBehaviour
 
     private void SpawnLevel4()
     {
-        // this level will choose a location to spawn the portal at multiple times throughout the game
+        
         List<GameObject> spawnedLillies = new List<GameObject>();
 
-        // get radius of each circle by position of first lillies
-        float[] radii = { lillies[0].transform.position.x, lillies[1].transform.position.x, lillies[2].transform.position.x };
+        
+        float[] circleRadii = { lillies[0].transform.position.x, lillies[1].transform.position.x, lillies[2].transform.position.x };
 
-        // loop through all three layers
+        
         for (int i = 0; i < numLillies.Length; i++)
         {
-            // to make the correct number of lillies in each layer
+            
             for (int j = 1; j < numLillies[i]; j++)
             {
-                // get x and y position of new lily (depends on number of lillies)
-                float xPos = radii[i] * Mathf.Cos(2 * j * Mathf.PI / numLillies[i]);
-                float yPos = radii[i] * Mathf.Sin(2 * j * Mathf.PI / numLillies[i]);
+                
+                float xPosLily = circleRadii[i] * Mathf.Cos(2 * j * Mathf.PI / numLillies[i]);
+                float yPosLily = circleRadii[i] * Mathf.Sin(2 * j * Mathf.PI / numLillies[i]);
 
-                // instantiate that object 
-                GameObject lily = Instantiate(lillies[i], new Vector3(xPos, yPos, 1), Quaternion.identity);
+                
+                GameObject lily = Instantiate(lillies[i], new Vector3(xPosLily, yPosLily, 1), Quaternion.identity);
 
-                // set it as a child of the water rings to make it rotate 
+                
                 lily.transform.parent = waterRings[i].transform;
                 lily.transform.localScale = lilyScales[i];
 
                 Lily = lily.GetComponent<lily>();
 
-                bool isEvil = Random.Range(0, 10) < 6;
+                bool isEvil = Random.Range(0, 10) < 3;
 
                 Lily.SetIsEvil(isEvil);
 
-                // add lily to array to access later
+                
                 spawnedLillies.Add(lily);
 
                 
             }
         }
 
-        // spawn the portal at random locations throughout the game
+        
         StartCoroutine(RespawnPortal(spawnedLillies));
     }
 
@@ -212,15 +213,15 @@ public class SpawnLilyPads : MonoBehaviour
         {
             yield return new WaitForSeconds(Random.Range(6, 10));
 
-            // reactivate portalLily + remove portal
+            
             portalLily.SetActive(true);
             Destroy(newPortal);
 
-            // choose a new lily to replace with portal
+            
             portalLily = lillies[(int)Mathf.Floor(Random.Range(0, lillies.Count - 1))];
             portalLily.SetActive(false);
 
-            // create a portal at that location
+            
             newPortal = Instantiate(portal, portalLily.transform.position, Quaternion.identity);
             newPortal.transform.parent = portalLily.transform.parent;
         }
@@ -228,25 +229,25 @@ public class SpawnLilyPads : MonoBehaviour
 
     private void SpawnLevel5()
     {
-        // create a list to store the lily game objects to spawn stars on
+        
         List<GameObject> spawnedLillies = new List<GameObject>();
 
-        // get radius of each circle by position of first lillies
-        float[] radii = { lillies[0].transform.position.x, lillies[1].transform.position.x, lillies[2].transform.position.x };
+        
+        float[] circleRadii = { lillies[0].transform.position.x, lillies[1].transform.position.x, lillies[2].transform.position.x };
 
-        // loop through all three layers
+        
         for (int i = 0; i < numLillies.Length; i++)
         {
-            // to make the correct number of lillies in each layer
+            
             for (int j = 1; j < numLillies[i]; j++)
             {
-                // get x and y position of new lily (depends on number of lillies)
-                float xPos = radii[i] * Mathf.Cos(2 * j * Mathf.PI / numLillies[i]);
-                float yPos = radii[i] * Mathf.Sin(2 * j * Mathf.PI / numLillies[i]);
+                
+                float xPos = circleRadii[i] * Mathf.Cos(2 * j * Mathf.PI / numLillies[i]);
+                float yPos = circleRadii[i] * Mathf.Sin(2 * j * Mathf.PI / numLillies[i]);
 
                 GameObject lily = null;
 
-                // choose what to spawn (lily, water, etc.)
+               
                 float probLily = Random.Range(0, 10);
                 if (probLily < 8.5)
                 {
@@ -255,10 +256,10 @@ public class SpawnLilyPads : MonoBehaviour
 
                 if (lily != null)
                 {
-                    // instantiate that object 
+                    
                     lily = Instantiate(lillies[i], new Vector3(xPos, yPos, 1), Quaternion.identity);
 
-                    // set it as a child of the water rings to make it rotate 
+                    
                     lily.transform.parent = waterRings[i].transform;
                     lily.transform.localScale = lilyScales[i];
 
@@ -273,12 +274,11 @@ public class SpawnLilyPads : MonoBehaviour
             }
         }
 
-        // spawn stars 
+         
         for (int k = 0; k < 2; k++)
         {
-            // randomly select lillies to spawn a star on
-            GameObject starLily1 = spawnedLillies[(int)Mathf.Floor(Random.Range(0, spawnedLillies.Count - 1))];
-            // spawn a star on that lily 
+            
+            GameObject starLily1 = spawnedLillies[(int)Mathf.Floor(Random.Range(0, spawnedLillies.Count - 1))]; 
             GameObject star1 = Instantiate(star, starLily1.transform.position, Quaternion.identity);
             star1.transform.parent = starLily1.transform;
         }
